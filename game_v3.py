34a5ca5835e37a5eb815c690eac1e3b5601415d2
Alexandr-Gorbushin <alegorbushin@gmail.com>
2023-06-02 11:16:32 +0500

    First commit

---
+++ game_v3.py
@@ -0,0 +1,88 @@
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
+
+import numpy as np
+
+
+def random_predict(number: int = 1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+
+    while True:
+        count += 1
+        predict_number=50
+        if number > predict_number:
+            predict_number=75
+            if number > predict_number:
+                predict_number=87
+                if number > predict_number:
+                    predict_number = np.random.randint(predict_number, 101)  # предполагаемое число
+                    if number == predict_number:
+                        break  # выход из цикла если угадали
+                elif number < predict_number:
+                    predict_number = np.random.randint(75, predict_number)  # предполагаемое число
+                    if number == predict_number:
+                        break  # выход из цикла если угадали
+                else:break
+            elif number < predict_number:
+                predict_number = np.random.randint(50, predict_number)  # предполагаемое число
+                if number == predict_number:
+                    break  # выход из цикла если угадали
+            else:break
+        elif number < predict_number:
+            predict_number=25
+            if number < predict_number:
+                predict_number=12
+                if number < predict_number:
+                    predict_number = np.random.randint(0, predict_number)  # предполагаемое число
+                    if number == predict_number:
+                        break  # выход из цикла если угадали
+                elif number > predict_number:
+                    predict_number = np.random.randint(predict_number, 25)  # предполагаемое число
+                    if number == predict_number:
+                        break  # выход из цикла если угадали
+                else:break
+            elif number > predict_number:
+                predict_number = np.random.randint(predict_number, 50)  # предполагаемое число
+                if number == predict_number:
+                    break  # выход из цикла если угадали
+            else:break
+          
+        else:break
+          
+        
+    return count
+
+
+def score_game(random_predict) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
